1ffd97aa9de4427ba8a3d4d5599b5a717823e892
e <1449430837@qq.com>
2021-04-12 19:09:58 +0800

    删除Test

--- PayrollSystem/src/main/java/com/tomorrow/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.tomorrow.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.tomorrow.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserDao extends BaseMapper<User> {
-}
