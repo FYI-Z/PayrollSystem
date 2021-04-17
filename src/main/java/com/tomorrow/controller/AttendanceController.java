d5c3b46e2bb06a0a23f76149bbb321420385713a
e <1449430837@qq.com>
2021-04-17 17:02:27 +0800

    实现考勤的增删查改功能

--- src/main/java/com/tomorrow/controller/AttendanceController.java
+++ src/main/java/com/tomorrow/controller/AttendanceController.java
@@ -10,11 +10,19 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/*
+ * @Author 李章
+ * @Description 考勤数据操作接口
+ * @Date 16:05 2021/4/17
+ **/
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/PayrollSystem/Attendance")
 public class AttendanceController {
@@ -23,15 +31,32 @@ public class AttendanceController {
     @ApiOperation(value = "考勤数据操作接口")
     @ApiImplicitParam()
 
-    @PostMapping("/showAll")
-    public ReturnResult showAll(@RequestParam int current, @RequestParam int size){
-        if(size == 0){
-            return ResultUtil.error(Constant.RESCODE_EXCEPTION,"参数错误");
+    //获取全部考勤记录
+    @ResponseBody
+    @PostMapping("/getAll")
+    public Map<String,Object> getAll(@RequestParam int page, @RequestParam int limit){
+        Map<String,Object> tableData =new HashMap<String,Object>();
+        if(page == 0 || limit ==0){
+            tableData.put("code", Constant.RESCODE_EXCEPTION);
+            tableData.put("msg", "error");
+            tableData.put("count", 0);
+            tableData.put("data", null);
+            return tableData;
         }
-        List<Attendance> attendanceList = attendanceService.showAll(current,size);
-        return ResultUtil.success(attendanceList,Constant.RESCODE_SUCCESS,attendanceList.size());
-    }
+        //获取全部考勤记录
+        List<Attendance> attendances = attendanceService.getAll();
+        //获取分页后的每页考勤记录
+        List<Attendance> attendanceList = attendanceService.getCount(page,limit);
 
+        tableData.put("code", Constant.RESCODE_SUCCESS);
+        tableData.put("msg", "success");
+        //将全部数据的条数作为count传给前台（一共多少条）
+        tableData.put("count", attendances.size());
+        //将分页后的数据返回（每页要显示的数据）
+        tableData.put("data", attendanceList);
+        return tableData;
+    }
+    //通过考勤ID获取考勤记录
     @PostMapping("/findById")
     public ReturnResult findAttendanceById(@RequestBody Attendance attendance){
        if (attendance.getAttendanceid() == null){
@@ -43,7 +68,19 @@ public class AttendanceController {
        }
        return ResultUtil.success(attendance, Constant.RESCODE_SUCCESS,1);
     }
-
+    //通过用户ID获取考勤记录
+    @PostMapping("/findByUserId")
+    public ReturnResult findAttendanceByUserId(@RequestBody Attendance attendance){
+        if (attendance.getUserid() == null){
+            return ResultUtil.error(Constant.RESCODE_EXCEPTION,"用户ID为空");
+        }
+        List<Attendance> attendanceList = attendanceService.findAttendanceByUserId(attendance.getUserid());
+        if(attendanceList.size() == 0){
+            return ResultUtil.error(Constant.RESCODE_NOEXIST,"查询结果为空");
+        }
+        return ResultUtil.success(attendanceList, Constant.RESCODE_SUCCESS,attendanceList.size());
+    }
+    //模糊查询
     @PostMapping("/findAll")
     public ReturnResult findAll(@RequestParam String key){
         List<Attendance> attendanceList = attendanceService.findAll(key);
@@ -66,12 +103,12 @@ public class AttendanceController {
     }
 
     @PostMapping("/del")
-    public ReturnResult delAttendance(@RequestBody List<String>  list){
-        if (list.size() == 0){
+    public ReturnResult delAttendance(String [] list){
+        if (list.length == 0){
             return ResultUtil.error(Constant.RESCODE_EXCEPTION,"没有需要删除的数据");
         }
         List<Attendance> attendanceList = attendanceService.delAttendance(list);
-        if(attendanceList == null){
+        if(attendanceList.size() == 0){
             return ResultUtil.error(Constant.RESCODE_DELETEERROR,"删除失败");
         }
         return ResultUtil.success(attendanceList, Constant.RESCODE_SUCCESS,attendanceList.size());
@@ -79,8 +116,8 @@ public class AttendanceController {
 
     @PostMapping("/add")
     public ReturnResult addAttendance(@RequestBody Attendance attendance){
-        if(attendance.getUserid() == null){
-            return ResultUtil.error(Constant.RESCODE_INSERTERROR,"用户ID为空，添加失败");
+        if(!isNumeric(attendance.getUserid())){
+            return ResultUtil.error(Constant.RESCODE_INSERTERROR,"用户ID只允许数字，添加失败");
         }
         attendance = attendanceService.addAttendance(attendance);
         if(attendance == null){
@@ -88,5 +125,19 @@ public class AttendanceController {
         }
         return ResultUtil.success(attendance, Constant.RESCODE_SUCCESS,1);
     }
-
+    /*
+     * @Author 李章
+     * @Description 判断是否为数字
+     * @Date 16:32 2021/4/17
+     * @Param [str]
+     * @return boolean
+     **/
+    public static boolean isNumeric(String str) {
+        Pattern pattern = Pattern.compile("-?[0-9]+.?[0-9]+");
+        Matcher isNum = pattern.matcher(str);
+        if (!isNum.matches()) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/com/tomorrow/service/AttendanceService.java
+++ src/main/java/com/tomorrow/service/AttendanceService.java
@@ -9,11 +9,13 @@ public interface AttendanceService {
     /**
      * 增删查改
      * */
-    public List<Attendance> showAll(int current, int size);
+    public List<Attendance> getAll();
+    public List<Attendance> getCount(int page, int limit);
     public List<Attendance> findAll(String key);
     public Attendance findAttendanceById(String attendanceid);
+    public List<Attendance> findAttendanceByUserId(String userid);
     public int updataAttendance(Attendance attendance);
-    public List<Attendance> delAttendance(List<String> list);
+    public List<Attendance> delAttendance(String [] list);
     public Attendance addAttendance(Attendance attendance);
 }
 
